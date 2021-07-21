305a7bd292dc8fb695515893b4d20ae05b135fa2
autoex <autoex@gmail.com>
2021-07-21 17:20:22 +0300

    code optimization

--- src/store-rdx-tkit/store.js
+++ src/store-rdx-tkit/store.js
@@ -2,12 +2,12 @@ import { configureStore,combineReducers } from '@reduxjs/toolkit'
 import {mainReducer} from "../store-rdx-tkit/main-reducer";
 import toolkitSlice from "../store/toolkitSlice";
 
-const rootReducer = combineReducers({
-    main: toolkitSlice
-});
+
 
 
 export const store = configureStore({
-    reducer:rootReducer
+    reducer: {
+        main: toolkitSlice
+    }
     }
 )
